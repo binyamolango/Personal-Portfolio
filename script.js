302295744e639287dd7f70c0fe4bcacc7a6d8136
MohammadYaser <yasersafi19@outlook.com>
2023-06-26 18:12:14 +0430

    Add display menu function

--- script.js
+++ script.js
@@ -14,3 +14,11 @@ closeImg.src = "img/ic_cross.svg";
 
 closeBtn.appendChild(closeImg);
 nav.appendChild(closeBtn);
+
+function displayMenu(e) {
+    menu.classList.toggle("hidden");
+    menuBtn.classList.toggle("hidden");
+    closeBtn.classList.toggle("hidden");
+    logo.classList.toggle("hidden");
+    logo2.classList.toggle("hidden");
+}
