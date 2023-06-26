65fd28e4d284d5b6a943c68a847cc3eb1f1fe2df
Binyam Y. Olango <125895809+binyamolango@users.noreply.github.com>
2023-06-26 16:40:04 +0300

    Update js file

--- script.js
+++ script.js
@@ -14,23 +14,3 @@ closeImg.src = "img/ic_cross.svg";
 
 closeBtn.appendChild(closeImg);
 nav.appendChild(closeBtn);
-
-function displayMenu(e) {
-    menu.classList.toggle("hidden");
-    menuBtn.classList.toggle("hidden");
-    closeBtn.classList.toggle("hidden");
-    logo.classList.toggle("hidden");
-    logo2.classList.toggle("hidden");
-}
-
-function closeMenu(event) {
-    menu.classList.toggle("hidden");
-    menuBtn.classList.toggle("hidden");
-    closeBtn.classList.toggle("hidden");
-    logo.classList.toggle("hidden");
-    logo2.classList.toggle("hidden");
-}
-
-menuBtn.addEventListener("click", displayMenu);
-closeBtn.addEventListener("click", closeMenu);
-navLink.addEventListener("click", closeMenu);
