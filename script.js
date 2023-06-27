15bbba9e4e40165607c50f10e80cb0d7b7c5b907
MohammadYaser <yasersafi19@outlook.com>
2023-06-27 18:31:28 +0430

    Add popup functions in script.js

--- script.js
+++ script.js
@@ -491,6 +491,30 @@ function popupWindow3() {
   }
 }
 
+function popupWindow4() {
+  if (window.screen.width <= 768) {
+    window.open('proj4-desc.html', 'popUp', 'width=400, height=400');
+  } else {
+    window.open('proj4-desc.html', 'popUp', 'width=1200, height=1000');
+  }
+}
+
+function popupWindow5() {
+  if (window.screen.width <= 768) {
+    window.open('proj5-desc.html', 'popUp', 'width=400, height=400');
+  } else {
+    window.open('proj5-desc.html', 'popUp', 'width=1200, height=1000');
+  }
+}
+
+function popupWindow6() {
+  if (window.screen.width <= 768) {
+    window.open('proj6-desc.html', 'popUp', 'width=400, height=400');
+  } else {
+    window.open('proj6-desc.html', 'popUp', 'width=1200, height=1000');
+  }
+}
+
 if (workBtn) {
   workBtn.addEventListener('click', popupWindow1);
 }
