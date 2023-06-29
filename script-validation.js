77a95820444e23bc56b42ea99131210bb5f3f446
user-faiz <email2faizm@gmail.com>
2023-06-29 16:32:56 +0530

    add form validation function

--- script-validation.js
+++ script-validation.js
@@ -1,4 +1,14 @@
 // contact validation script
 document.getElementById('form').addEventListener('submit', (e) => {
     const emailVal = document.getElementById('email').value;
+    const emailErr = document.getElementById('error_message');
+    const validate = emailVal.toLowerCase();
+    const form = document.getElementById('form');
+    if (emailVal === validate) {
+      emailErr.textContent = '';
+      form.submit();
+    } else {
+      e.preventDefault();
+      emailErr.textContent = 'Please write your email in lower case!';
+    }
   });
