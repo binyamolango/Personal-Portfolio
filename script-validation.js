b5c8d8a7a0335bb49616c4e3b781be3b7ce506eb
Binyam Y. Olango <125895809+binyamolango@users.noreply.github.com>
2023-06-29 13:56:08 +0300

    Create contact validation js file

--- script-validation.js
+++ script-validation.js
@@ -1,13 +1,1 @@
-document.getElementById('form').addEventListener('submit', (e) => {
-  const emailVal = document.getElementById('email').value;
-  const emailErr = document.getElementById('error_message');
-  const validate = emailVal.toLowerCase();
-  const form = document.getElementById('form');
-  if (emailVal === validate) {
-    emailErr.textContent = '';
-    form.submit();
-  } else {
-    e.preventDefault();
-    emailErr.textContent = 'Please write your email in lower case!';
-  }
-});
+// contact validation script
