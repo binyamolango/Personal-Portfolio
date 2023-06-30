d6a49ad926a2c79da616b80af019ed4c8f89d696
Binyam Y. Olango <125895809+binyamolango@users.noreply.github.com>
2023-06-30 11:29:41 +0300

    Update script-preserveData.js file

--- script-preserveData.js
+++ script-preserveData.js
@@ -1,20 +1,2 @@
 // store data
 
-const form = document.getElementById('form');
-const data = {
-    name : document.getElementById("name").value,
-    email : document.getElementById("email").value,
-    text : document.getElementById("msg").value
-};
-
-function storeData() {
-    localStorage.setItem('form', JSON.stringify(data));
-}
-
-form.addEventListener('input', storeData);
-
-const formObj = JSON.parse(localStorage.getItem('form'));
-
-document.getElementById('name').value = formObj.name;
-document.getElementById('email').value = formObj.email;
-document.getElementById('msg').value = formObj.text;
