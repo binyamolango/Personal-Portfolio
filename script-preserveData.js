36ff63b73d70a6a10c446e5f856bd04cb9af3db3
MohammadYaser <yasersafi19@outlook.com>
2023-06-30 13:10:09 +0430

    Add data object

--- script-preserveData.js
+++ script-preserveData.js
@@ -1,2 +1,7 @@
 // store data
 const form = document.getElementById('form');
+const data = {
+    name : document.getElementById("name").value,
+    email : document.getElementById("email").value,
+    text : document.getElementById("msg").value
+};
