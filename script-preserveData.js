fbbd7f2a3caa1fcca309fe5ebeab8f52c0e6ed5a
user-faiz <email2faizm@gmail.com>
2023-07-02 01:14:45 +0530

    update eslint errors

--- script-preserveData.js
+++ script-preserveData.js
@@ -1,16 +1,15 @@
-
 const nameInput = document.getElementById('name');
 const emailInput = document.getElementById('email');
 const messageInput = document.getElementById('msg');
 const storageKey = 'formData';
 
 function saveFormData() {
-const formData = {
-name: nameInput.value,
-email: emailInput.value,
-message: messageInput.value
-};
-localStorage.setItem(storageKey, JSON.stringify(formData));
+  const formData = {
+    name: nameInput.value,
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem(storageKey, JSON.stringify(formData));
 }
 
 nameInput.addEventListener('input', saveFormData);
@@ -18,11 +17,11 @@ emailInput.addEventListener('input', saveFormData);
 messageInput.addEventListener('input', saveFormData);
 
 window.addEventListener('load', () => {
-const savedData = localStorage.getItem(storageKey);
-if (savedData) {
-const formData = JSON.parse(savedData);
-nameInput.value = formData.name;
-emailInput.value = formData.email;
-messageInput.value = formData.message;
-}
+  const savedData = localStorage.getItem(storageKey);
+  if (savedData) {
+    const formData = JSON.parse(savedData);
+    nameInput.value = formData.name;
+    emailInput.value = formData.email;
+    messageInput.value = formData.message;
+  }
 });
